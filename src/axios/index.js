3949cafbfdd331f6601ed26375a87dd5a2d68757
Franciscom91 <73622322+Franciscom91@users.noreply.github.com>
2021-05-11 20:46:51 -0300

    URL changed

--- src/axios/index.js
+++ src/axios/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: 'https://backlanding.herokuapp.com/api/'
+    baseURL: 'https://backlanding.herokuapp.com/contact/'
 })
 
 const sendEmail = async (data) => {
